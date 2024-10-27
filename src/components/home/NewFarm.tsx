e5e34f92e25efe0d8f47d6b6fa453aed15590898
ahmadou5 <ahmadlasauwal@gmail.com>
2024-10-27 19:27:21 +0100

    new22rerej3

--- src/components/home/NewFarm.tsx
+++ src/components/home/NewFarm.tsx
@@ -451,7 +451,7 @@ export default function NewApp() {
       {/*** services  */}
       <ServicesSection />
       {/* Company Profile Section */}
-      <section className="py-16 bg-white">
+      <section className="py-16 px-2 bg-white">
         <div className="container mx-auto px-4">
           <h2 className="text-3xl font-bold text-center mb-12">
             Company Profile
